039fe4ab508586c21500f91edb367067231053bc
Hammed Oyedele <itz.harmid@gmail.com>
2020-05-30 09:01:02 +0100

    feat: add bang to greeting

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const HOST = ENV.HOST ?? 'localhost'
 const PORT = parseInt(ENV.PORT ?? 3000)
 
 listenAndServe({ hostname: HOST, port: PORT }, (req) => {
-  req.respond({ body: 'Hello World' })
+  req.respond({ body: 'Hello World!' })
 })
 
 console.log(`Server running on http://${HOST}:${PORT}`)
